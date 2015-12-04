c709079efef2805304f29923d63616777e1fc9cb
xuwq2015 <xuwq2015@163.com>
2015-12-04 18:30:30 +0800

    发送接收并处理带参信号

---
+++ sigqueue.c
@@ -0,0 +1,29 @@
+/* 子进程发送一个带参信号，父进程接收并处理这个带参信号 */
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void func(int sig, siginfo_t *info, void *p) {
+	printf("收到信号，来自用户%d的进程%d，附带参数为%d\n", info->si_uid,
+		info->si_pid, info->si_value);
+}
+
+int main() {
+	struct sigaction act, oldact;
+
+	act.sa_sigaction = func;	//设置带参信号处理函数
+	sigemptyset(&act.sa_mask);	//不用屏蔽什么信号
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR2, &act, &oldact);	//登记信号处理
+
+	if(fork() ==0) {
+		union sigval v;
+		printf("uid=%d, pid=%d\n", getuid(), getpid());
+		v.sival_int = 2013;	//设置要传递的参数
+		sigqueue(getppid(), SIGUSR2, v);//发送信号和参数
+		sleep(1);
+	}else {
+		pause();
+	}
+	return 0;
+}
